280bd12e10bbf71a89eae1c4b119e4c7e93693f8
hemantksingh <hemantksingh.hk@gmail.com>
2022-08-05 00:28:27 +0100

    Add documentation

--- provisioning/apigateway.tf
+++ provisioning/apigateway.tf
@@ -1,10 +1,10 @@
-resource "aws_apigatewayv2_api" "lambda" {
+resource "aws_apigatewayv2_api" "public_apigateway" {
   name          = "${local.stack_name}-gw"
   protocol_type = "HTTP"
 }
 
 resource "aws_apigatewayv2_stage" "lambda" {
-  api_id = aws_apigatewayv2_api.lambda.id
+  api_id = aws_apigatewayv2_api.public_apigateway.id
 
   name        = "brewbar"
   auto_deploy = true
@@ -30,7 +30,7 @@ resource "aws_apigatewayv2_stage" "lambda" {
 
 # configure api gateway to use the orders lambda
 resource "aws_apigatewayv2_integration" "orders" {
-  api_id = aws_apigatewayv2_api.lambda.id
+  api_id = aws_apigatewayv2_api.public_apigateway.id
 
   integration_uri    = aws_lambda_function.orders.invoke_arn
   integration_type   = "AWS_PROXY"
@@ -39,7 +39,7 @@ resource "aws_apigatewayv2_integration" "orders" {
 
 # map HTTP GET request to a target: lambda 
 resource "aws_apigatewayv2_route" "orders" {
-  api_id = aws_apigatewayv2_api.lambda.id
+  api_id = aws_apigatewayv2_api.public_apigateway.id
 
   route_key = "GET /orders"
   target    = "integrations/${aws_apigatewayv2_integration.orders.id}"
@@ -47,7 +47,7 @@ resource "aws_apigatewayv2_route" "orders" {
 
 # define a log group to store access logs for the aws_apigatewayv2_stage.lambda API Gateway stage
 resource "aws_cloudwatch_log_group" "api_gw" {
-  name = "/aws/apigateway/${aws_apigatewayv2_api.lambda.name}"
+  name = "/aws/apigateway/${aws_apigatewayv2_api.public_apigateway.name}"
 
   retention_in_days = 30
 }
@@ -60,5 +60,5 @@ resource "aws_lambda_permission" "api_gw" {
   function_name = aws_lambda_function.orders.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
+  source_arn = "${aws_apigatewayv2_api.public_apigateway.execution_arn}/*/*"
 }
